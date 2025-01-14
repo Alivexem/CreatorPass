c6a97b394d054adb2d7d9e5e04c08c19bb48e87d
alivexem <athkinstestimony@gmail.com>
2025-01-14 19:11:13 +0100

    Style: Responsiveness for Auth completed

--- app/Signup.tsx
+++ app/Signup.tsx
@@ -11,7 +11,7 @@ const App = () => {
     const router = useRouter();
     const { address, isConnected, caipAddress, status, embeddedWalletInfo } = useAppKitAccount();
     const { open } = useAppKit();
-    
+
     useEffect(() => {
         if (isConnected) {
             setShowToaster(true);
@@ -21,8 +21,8 @@ const App = () => {
             return () => clearTimeout(timer);
         }
     }, [isConnected]);
-    
-    const openModal = async() => {
+
+    const openModal = async () => {
         try {
             if (isConnected) {
                 localStorage.setItem('address', address as string);
@@ -30,27 +30,37 @@ const App = () => {
                 return
             }
             await open();
-            
-          } catch (error) {
+
+        } catch (error) {
             console.error('Modal error:', error);
             // Handle error appropriately
-          }
-        
-      };
+        }
+
+    };
     return (
-        <div className='flex min-h-screen md:-mt-[70px] w-full items-center justify-center bg-[#1A1D1F] text-white px-[20px] lg:px-[40px] lg:ml-10'>
+        <div className='flex min-h-screen m-0 p-0 w-full items-center justify-center bg-[#1A1D1F] text-white'>
             {showToaster && (
                 <div className='absolute top-5 right-5 bg-blue-500 text-white px-4 py-2 rounded'>
                     Authentication Succesfull
                 </div>
             )}
             <div className='flex flex-col items-center lg:items-start max-w-[600px] w-full'>
-                <div className='my-14'>
-                    <Image 
-                        src='/aiwoman.jpeg' 
-                        className='rounded-lg lg:hidden' 
-                        alt='aiwoman' 
-                        height={250} 
+                <div style={{
+                    background: 'linear-gradient(93.1deg, #6d2baa 0%, #682E9E 98.22%)',
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    width: '100%'
+                }} className='pt-32 pb-20 mb-14 flex justify-center items-center md:hidden'>
+                    <div className='absolute top-5 left-5 md:hidden'>
+                        <Image src='/whiteLogo.png' alt='logo' height={30} width={150} />
+                    </div>
+                    <Image
+                        src='/aiwoman.jpeg'
+                        className='rounded-lg lg:hidden'
+                        alt='aiwoman'
+                        height={250}
                         width={250}
                         style={{
                             animation: 'lightPulse 3s infinite'
@@ -70,20 +80,20 @@ const App = () => {
                         }
                     `}</style>
                 </div>
-                <div className='text-[2.5rem] md:text-[30px] text-white font-bold w-full text-center lg:text-left mb-4'>
+                <div className='text-[2.5rem] mt-[500px] md:mt-0 md:text-[30px] text-white font-bold w-full text-left lg:text-left mb-4 pl-4'>
                     <p>CreatorPass</p>
                 </div>
-                <p className='text-center lg:text-left mb-5'>
+                <p className='text-left lg:text-left mb-5 pl-4'>
                     CreatorPass connects creators and fans through unique NFT passes. Creators can easily mint digital passes to share premium content, while fans unlock exclusive videos, events, and more with their passes.</p>
-                <div className='text-[2.5rem] md:text-[30px] text-white font-bold w-full text-center lg:text-left mb-5 mt-5'>
+                <div className='text-[2.5rem] md:text-[30px] text-white font-bold w-full text-left lg:text-left mb-5 mt-5 pl-4'>
                     <p className='leading-none'>For creators</p>
                 </div>
-                <p className='text-center lg:text-left mb-5'>Monetize content, manage fans, and offer special experiences.</p>
-                <div className='text-[2.5rem] md:text-[30px] text-white font-bold w-full text-center lg:text-left mb-5 mt-5'>
+                <p className='text-left lg:text-left mb-5 pl-4'>Monetize content, manage fans, and offer special experiences.</p>
+                <div className='text-[2.5rem] md:text-[30px] text-white font-bold w-full text-left lg:text-left mb-5 mt-5 pl-4'>
                     <p className='leading-none'>For fans</p>
                 </div>
-                <p className='text-center lg:text-left mb-5'>Get exclusive access to the content you love and connect deeper with creators.</p>
-                <div style={{backgroundColor: isConnected ? '#34c759' : '#7c3aed'}} className='w-full flex justify-center mb-14 md:mb-0 mt-6'>
+                <p className='text-left lg:text-left mb-5 pl-4'>Get exclusive access to the content you love and connect deeper with creators.</p>
+                <div style={{ backgroundColor: isConnected ? '#34c759' : '#7c3aed' }} className='w-full flex justify-center mb-14 md:mb-0 mt-6'>
                     <button onClick={() => openModal()} className='w-full max-w-[300px] rounded-lg flex items-center justify-center gap-x-2 h-[50px] text-white'>
                         <Image src='/sol.png' alt='sol' height={20} width={20} /> {isConnected ? 'Go in' : 'Get started'}
                     </button>
