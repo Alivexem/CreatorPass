4f0181c963d2d72437628d26594c0be804269774
alivexem <athkinstestimony@gmail.com>
2025-01-14 18:38:30 +0100

    style: Auth completed

--- app/Signup.tsx
+++ app/Signup.tsx
@@ -1,9 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react'
-// import { PiEyeClosedThin } from "react-icons/pi";
-// import { GoEye } from "react-icons/go";
 import Image from 'next/image';
-// import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 import { useAppKit, useAppKitAccount } from '../utils/reown';
@@ -41,27 +38,56 @@ const App = () => {
         
       };
     return (
-        <div className='relative lg:ml-10 mt-24 lg:mt-[80px] bg-[#1A1D1F] text-white px-[40px] flex flex-col justify-center items-center lg:items-start'>
+        <div className='flex min-h-screen md:-mt-[70px] w-full items-center justify-center bg-[#1A1D1F] text-white px-[20px] lg:px-[40px] lg:ml-10'>
             {showToaster && (
                 <div className='absolute top-5 right-5 bg-blue-500 text-white px-4 py-2 rounded'>
-                    Account created successfully
+                    Authentication Succesfull
                 </div>
             )}
-            <div className='md:text-[30px] text-[3rem] text-white font-bold w-full text-center lg:text-left mb-4'>
-                <p> CreatorPass</p>
-            </div>
-            <p className='mb-5'>
-                CreatorPass connects creators and fans through unique NFT passes. Creators can easily mint digital passes to share premium content, while fans unlock exclusive videos, events, and more with their passes. </p>
-             <div className='md:text-[30px] text-[3rem] text-white font-bold w-full text-center lg:text-left mb-5 mt-5'>
-                <p className='leading-none'>  For creators</p>
-            </div>
-             <p className='mb-5'>Monetize content, manage fans, and offer special experiences. </p>
-              <div className='md:text-[30px] text-[3rem] text-white font-bold w-full text-center lg:text-left mb-5 mt-5'>
-                <p className='leading-none'>  For fans</p>
-            </div>
-            <p className='mb-5'> Get exclusive access to the content you love and connect deeper with creators. </p>
-            <div style={{backgroundColor: isConnected ? '#34c759' : '#7c3aed'}} className='my-[24px] mt-[10px] flex flex-col lg:flex-row cursor-pointer justify-center lg:justify-center space-y-4 lg:space-y-0 lg:space-x-3 items-center w-full p-2'>
-                <button onClick={() => openModal()} className='w-[70%] lg:w-[300px] rounded-lg flex items-center justify-center gap-x-2 h-[50px] text-white'><Image src='/sol.png' alt='sol' height={20} width={20} /> {isConnected ? 'Proceed' : 'Get started'}</button>
+            <div className='flex flex-col items-center lg:items-start max-w-[600px] w-full'>
+                <div className='my-14'>
+                    <Image 
+                        src='/aiwoman.jpeg' 
+                        className='rounded-lg lg:hidden' 
+                        alt='aiwoman' 
+                        height={250} 
+                        width={250}
+                        style={{
+                            animation: 'lightPulse 3s infinite'
+                        }}
+                    />
+                    <style jsx global>{`
+                        @keyframes lightPulse {
+                            0% {
+                                opacity: 1;
+                            }
+                            50% {
+                                opacity: 0.7;
+                            }
+                            100% {
+                                opacity: 1;
+                            }
+                        }
+                    `}</style>
+                </div>
+                <div className='text-[2.5rem] md:text-[30px] text-white font-bold w-full text-center lg:text-left mb-4'>
+                    <p>CreatorPass</p>
+                </div>
+                <p className='text-center lg:text-left mb-5'>
+                    CreatorPass connects creators and fans through unique NFT passes. Creators can easily mint digital passes to share premium content, while fans unlock exclusive videos, events, and more with their passes.</p>
+                <div className='text-[2.5rem] md:text-[30px] text-white font-bold w-full text-center lg:text-left mb-5 mt-5'>
+                    <p className='leading-none'>For creators</p>
+                </div>
+                <p className='text-center lg:text-left mb-5'>Monetize content, manage fans, and offer special experiences.</p>
+                <div className='text-[2.5rem] md:text-[30px] text-white font-bold w-full text-center lg:text-left mb-5 mt-5'>
+                    <p className='leading-none'>For fans</p>
+                </div>
+                <p className='text-center lg:text-left mb-5'>Get exclusive access to the content you love and connect deeper with creators.</p>
+                <div style={{backgroundColor: isConnected ? '#34c759' : '#7c3aed'}} className='w-full flex justify-center mb-14 md:mb-0 mt-6'>
+                    <button onClick={() => openModal()} className='w-full max-w-[300px] rounded-lg flex items-center justify-center gap-x-2 h-[50px] text-white'>
+                        <Image src='/sol.png' alt='sol' height={20} width={20} /> {isConnected ? 'Go in' : 'Get started'}
+                    </button>
+                </div>
             </div>
         </div>
     )
