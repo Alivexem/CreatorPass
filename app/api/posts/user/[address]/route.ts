2ef99fd92f8f80e4c997ede2f7de8d1ed6c03f00
alivexem <athkinstestimony@gmail.com>
2025-01-24 16:21:22 +0100

    Deployment err2

--- app/api/posts/user/[address]/route.ts
+++ app/api/posts/user/[address]/route.ts
@@ -2,13 +2,19 @@ import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/libs/mongodb';
 import Creates from '@/models/uploads';
 
+type Props = {
+    params: {
+        address: string
+    }
+}
+
 export async function GET(
     request: NextRequest,
-    { params }: { params: { address: string } }
+    context: Props
 ) {
     try {
         await connectDB();
-        const address = params.address;
+        const address = context.params.address;
 
         const posts = await Creates.find({ username: address })
             .sort({ createdAt: -1 });
