29aefed92d518ab2af68739f8f871f210a99b869
alivexem <athkinstestimony@gmail.com>
2025-01-24 16:16:43 +0100

    Deployment error handled

--- app/api/posts/user/[address]/route.ts
+++ app/api/posts/user/[address]/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/libs/mongodb';
 import Creates from '@/models/uploads';
 
 export async function GET(
-    request: Request,
+    request: NextRequest,
     { params }: { params: { address: string } }
 ) {
     try {
