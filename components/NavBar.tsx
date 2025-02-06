b493cce8da6bfd9891ca7706eddd56c2d42c7b96
alivexem <athkinstestimony@gmail.com>
2025-02-06 09:17:03 +0100

    style: more fix added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -111,7 +111,7 @@ const NavBar = () => {
         </div>
       )}
       <div style={{ background: 'radial-gradient(circle, #8748c2 0%, #682E9E 98.22%)' }} className='p-3 z-50 flex justify-between position-fixed top-0 items-center'>
-        <Image src='/whiteLogo.png' alt='logo' height={40} width={200} />
+        <Image src='/whiteLogo.png' alt='logo' height={27} width={120} className='md:h-[40px] md:w-[200px]' />
 
         <form onSubmit={handleSearch} className='lg:h-14 hidden md:flex md:h-14 lg:w-[400px] md:w-[40%] p-2 bg-gray-200 rounded-[40px] items-center'>
 
