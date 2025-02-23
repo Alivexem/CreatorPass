ec5de7586accfa3a673bf700f2c94c0b40b74b1a
Athkins Emmanuel Testimony <athkinstestimony@gmail.com>
2025-02-23 11:04:47 +0100

    Update index.tsx

--- app/context/index.tsx
+++ app/context/index.tsx
@@ -1,46 +1,59 @@
+'use client';
+
 import { createAppKit } from '@reown/appkit/react';
 import { SolanaAdapter } from '@reown/appkit-adapter-solana/react';
-import { solanaTestnet, solanaDevnet } from '@reown/appkit/networks';
+import { solana, solanaTestnet, solanaDevnet } from '@reown/appkit/networks';
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+
+// import { useAppKitConnection } from '@reown/appkit-adapter-solana/react';
+// import { PublicKey, Transaction, SystemProgram } from "@solana/web3.js";
+// import { useAppKitAccount, useAppKitProvider } from '@reown/appkit/react';
+// import type { Provider } from '@reown/appkit-adapter-solana/react';
+
 import React, { ReactNode } from 'react';
 
+// Retrieve the project ID from environment variables
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || '2927c2ac9dcae72c2006b80e814ac6e4';
 
 if (!projectId) {
   throw new Error('Project ID is not defined.');
 }
 
+// Set up Solana adapter with wallet adapters
 const solanaAdapter = new SolanaAdapter({
   wallets: [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
 });
 
+// Define metadata for your application
 const metadata = {
   name: 'AppKit',
   description: 'AppKit Solana Example',
-  url: 'https://creatorpass.vercel.app',
+  url: 'https://creatorpass.vercel.app', // Ensure this matches your app's URL
   icons: ['https://avatars.githubusercontent.com/u/179229932'],
 };
-
+// Initialize AppKit with the Solana adapter and network configurations
 export const modal = createAppKit({
   adapters: [solanaAdapter],
   projectId,
-  networks: [solanaTestnet, solanaDevnet],
+
+  networks: [ solanaTestnet, solanaDevnet],
   metadata,
   features: {
-    email: false, // Disable email login
-    socials: false, // Disable social logins
+    email: false,
+    socials: [],
     emailShowWallets: false,
-    swaps: true,
+    swaps: true, 
     onramp: true,
   },
   allWallets: 'SHOW',
   themeMode: 'dark',
   themeVariables: {
     '--w3m-color-mix': '#4F6E85',
-    '--w3m-color-mix-strength': 30,
-  },
+    '--w3m-color-mix-strength': 30
+  }
 });
 
+// Context provider component
 const ContextProvider = ({ children }: { children: ReactNode }) => {
   return children;
 };
