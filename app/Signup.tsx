3ea9a63c3e062b6dfc83f434c4bb0dcc43b581b8
Athkins Emmanuel Testimony <athkinstestimony@gmail.com>
2025-02-23 11:15:22 +0100

    Update Signup.tsx

--- app/Signup.tsx
+++ app/Signup.tsx
@@ -81,7 +81,7 @@ const App = () => {
                         <p>CreatorPass</p>
                     </div>
                     <p className='text-left lg:text-left mb-5 pl-4 text-gray-300'>
-                        CreatorPass connects creators and fans through unique NFT passes. Creators can easily mint digital passes to share premium content, while fans unlock exclusive videos, events, and more with their passes. Earn CRTP points that will later be converted into Tradable tokens when you engage with posts and mint creators pass</p>
+                        CreatorPass connects creators and fans through unique NFT passes. Creators can easily mint digital passes to share premium content, while fans unlock exclusive videos, events, and more with their passes. Earn CRTP points that will later be converted into Tradable tokens when you engage with posts and mint NFT passes</p>
                     <div className='text-[2.5rem] flex items-center gap-x-2 md:text-[30px] text-white font-bold w-full text-left lg:text-left mb-5 mt-5 pl-4'>
                         {/* <FcLinux /> */}
                         <p className='leading-none'>For creators</p>
@@ -91,7 +91,7 @@ const App = () => {
                         {/* <FcLike /> */}
                         <p className='leading-none'>For fans</p>
                     </div>
-                    <p className='text-left lg:text-left mb-5 pl-4 text-gray-300'>Get exclusive access to the content you love and connect deeper with creators. Gift creators directly and buy crypto effortlessly to top-up balance</p>
+                    <p className='text-left lg:text-left mb-5 pl-4 text-gray-300'>Get exclusive access to the content you love and connect deeper with creators. Gift creators directly and buy crypto effortlessly in-app to top-up balance</p>
 
                 </div>
 
@@ -101,7 +101,7 @@ const App = () => {
                             <p>CreatorPass</p>
                         </div>
                         <p className='text-left lg:text-left mb-5 pl-4 text-gray-300'>
-                            CreatorPass connects creators and fans through unique NFT passes. Creators can easily mint digital passes to share premium content, while fans unlock exclusive videos, events, and more with their passes. Earn CRTP points that will later be converted into Tradable tokens when you engage with posts and mint creators pass</p>
+                            CreatorPass connects creators and fans through unique NFT passes. Creators can easily mint digital passes to share premium content, while fans unlock exclusive videos, events, and more with their passes. Earn CRTP points that will later be converted into Tradable tokens when you engage with posts and mint NFT passes</p>
                     </div>
 
                     <div className='flex flex-col justify-center items-center border border-purple-500 rounded-xl bg-transparent p-4 mt-7'>
@@ -116,7 +116,7 @@ const App = () => {
                         {/* <FcLike /> */}
                         <p className='leading-none'>For fans</p>
                     </div>
-                        <p className='text-left lg:text-left mb-5 pl-4 text-gray-300'>Get exclusive access to the content you love and connect deeper with creators. Gift creators directly and buy crypto effortlessly to top-up balance.</p>
+                        <p className='text-left lg:text-left mb-5 pl-4 text-gray-300'>Get exclusive access to the content you love and connect deeper with creators. Gift creators directly and buy crypto effortlessly in-app to top-up balance.</p>
                     </div>
                 </div>
                 <div onClick={() => openModal()} style={{ backgroundColor: isConnected ? '#34c759' : '#7c3aed' }} className='w-full rounded-lg cursor-pointer md:ml-4 flex justify-center mb-14 md:mb-0 mt-6'>
