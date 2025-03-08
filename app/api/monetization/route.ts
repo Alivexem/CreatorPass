8d97d23e26aa48a14d9234c4d8bf6cb08dfcabd9
alivexem <athkinstestimony@gmail.com>
2025-03-08 09:15:26 +0100

    schema correction

--- app/api/monetization/route.ts
+++ app/api/monetization/route.ts
@@ -1,17 +1,17 @@
 import connectDB from "@/libs/mongodb";
 import { NextResponse } from 'next/server';
-import Monetization from '@/models/monetization'; // You'll need to create this model
+import monetization from '@/models/monetization'; // You'll need to create this model
 
 // Connect to MongoDB once for the module
 await connectDB();
 
 export async function GET() {
     try {
-        const monetization = await Monetization.findOne({
+        const money = await monetization.findOne({
             _id: "67c57a753756f665413fef96"
         });
 
-        if (!monetization) {
+        if (!money) {
             return new NextResponse(
                 JSON.stringify({ error: 'Monetization address not found' }),
                 { status: 404 }
@@ -19,7 +19,7 @@ export async function GET() {
         }
 
         return new NextResponse(
-            JSON.stringify({ address: monetization.account }),
+            JSON.stringify({ address: money.account }),
             { status: 200 }
         );
     } catch (error) {
