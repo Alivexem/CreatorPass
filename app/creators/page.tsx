2ba4b13022864de5bde950adeadc2af9a4f5edb4
alivexem <athkinstestimony@gmail.com>
2025-03-17 08:38:07 +0100

    another push

--- app/creators/page.tsx
+++ app/creators/page.tsx
@@ -1,12 +1,4 @@
 'use client'
-// Remove these lines
-// export const dynamic = 'force-dynamic';
-// export const revalidate = 0;
-
-// Add metadata export
-export const metadata = {
-    dynamic: 'force-dynamic'
-};
 
 import React, { useState, useEffect, TouchEvent, useRef, ReactNode } from 'react'
 import NavBar from '@/components/NavBar'
