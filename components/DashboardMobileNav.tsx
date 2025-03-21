0439df4dc5b4befd86f8350c3ebf0580e24536da
alivexem <athkinstestimony@gmail.com>
2025-03-21 17:03:59 +0100

    athkins

--- components/DashboardMobileNav.tsx
+++ components/DashboardMobileNav.tsx
@@ -33,7 +33,7 @@ const DashboardMobileNav = ({
   };
 
   return (
-    <div className='h-[80px] px-4 border-purple-600 border-t pt-3 box-border flex items-center justify-between w-full bg-black fixed bottom-0 text-white md:hidden z-50'>
+    <div className='h-[80px] px-4 border-purple-600 border-t pt-3 box-border flex items-center justify-between w-full bg-black pb-3 fixed bottom-0 text-white md:hidden z-50'>
       <Link href='/welcome'>
         <div className='flex flex-col cursor-pointer items-center space-y-2'>
           <GoHomeFill className="text-gray-300" />
